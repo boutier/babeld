e4add7686a02a1b1c008a3671d686070b473865e
Matthieu Boutier <boutier@pps.univ-paris-diderot.fr>
2013-08-12 17:39:27 +0200

    Add option to print the babel version number.

--- babeld.h
+++ babeld.h
@@ -20,6 +20,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
+#include "version.h"
+
 #define INFINITY ((unsigned short)(~0))
 
 #ifndef RTPROT_BABEL
